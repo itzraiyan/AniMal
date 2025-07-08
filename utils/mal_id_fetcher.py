4c09979cb28de67c7d9f229d1862a435b544963b
itzraiyan <mdzilhazzarefin@gmail.com>
2025-07-09 01:23:04 +0600

    Update mal_id_fetcher.py

--- utils/mal_id_fetcher.py
+++ utils/mal_id_fetcher.py
@@ -1,3 +1,4 @@
+# utils/mal_id_fetcher.py
 import requests
 from config import constants
 from utils.cli_helpers import print_error
@@ -5,7 +6,6 @@ from utils.cli_helpers import print_error
 def get_mal_id_from_username(username):
     if not username:
         return "0"
-    
     url = constants.MAL_USER_ID_API.format(username=username)
     try:
         resp = requests.get(url, timeout=10)
